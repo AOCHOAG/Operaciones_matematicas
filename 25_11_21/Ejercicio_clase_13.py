f54c1c2ff297e29dbfe0a3338f6f62a2e32423bc
AOCHOAG <aochoag@alumnos.nebrija.es>
2021-11-25 15:50:00 +0100

    Ejercicio 13_clase Working

--- 25_11_21/Ejercicio_clase_13.py
+++ 25_11_21/Ejercicio_clase_13.py
@@ -1,17 +1,25 @@
-import StatsData as sd
+import stats_data as sd
 
-res = sd.StatsData([1,2,8.5,3.1])
-print("Lista de numeros: ", res.l_data)
-print("Media: ", res.mean)
-print("Mediana: ", res.median)
-print("Varianza: ", res.varance)
+# res = sd.StatsData([1,2,8.5,3.1])
+# print("Lista de numeros: ", res.l_data)
+# print("Media: ", res.mean)
+# print("Mediana: ", res.median)
+# print("Varianza: ", res.varance)
 
 lista = []
 while True:
-    numeros = float(input("Introduce numeros: "))
-    if numeros = -999:
+    numeros = input("Introduce numeros: ")
+    if numeros == "fin":
         break
+    numeros = float(numeros)
     lista.append(numeros)
-lista_numeros = sd.lista(mean, media, mediana)
-print(sd.mean(lista))
+calculo_stats = sd.StatsData(lista) #Creo una variable de tipo objeto calculo_stats
+                                    #El objeto  creado de tipo sd.StatsData se almacena
+                                    #en la variable calculo_stats
+
+print("Lista de numeros: ", calculo_stats.l_data) #Se trata de una lista dentro de un objeto
+                                                  #l_data es un
+print("Media: ", calculo_stats.mean)
+print("Mediana: ", calculo_stats.median)
+print("Varianza: ", calculo_stats.varance)
 
